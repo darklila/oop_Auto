0b946acfe557f878c91169a3e7f43c5a1d9fd101
darklila <darklila@web.de>
2016-03-04 12:12:59 +0100

    Initial Commit

---
+++ oop_Auto/src/de/darklila/auto/Auto.java
@@ -0,0 +1,12 @@
+package de.darklila.auto;
+
+public class Auto {
+	private String name;
+	private int geschwindigkeit;
+
+	public Auto(String name) {
+		this.name=name;
+		this.geschwindigkeit=0;
+	}
+
+}
---
+++ oop_Auto/src/de/darklila/auto/Main.java
@@ -0,0 +1,11 @@
+package de.darklila.auto;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Auto meinAuto = new Auto("Blaui");
+		
+
+	}
+
+}

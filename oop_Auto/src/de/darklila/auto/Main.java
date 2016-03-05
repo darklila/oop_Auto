35bb65d0327f61dc98bccfaf1fee3fdd752cf43b
darklila <darklila@web.de>
2016-03-05 19:56:55 +0100

    UML Kommentar eingefügt

--- oop_Auto/src/de/darklila/auto/Main.java
+++ oop_Auto/src/de/darklila/auto/Main.java
@@ -1,5 +1,52 @@
 package de.darklila.auto;
 
+
+/**
+ * @startuml
+ * class Programm {
+ * 	main(args:String[]) : void
+ * }
+ * 
+ * class Auto {
+ * 	geschwindigkeit : int
+ * }
+ * 
+ * Programm - Auto
+ * 
+ * class Rad {
+ * 	drehen : boolean
+ * }
+ * 
+ * class Vorderrad {
+ * 	richtung : int
+ * }
+ * 
+ * Auto - "2" Vorderrad
+ * Rad "2" <|-- Vorderrad 
+ *  
+ * class Lenkrad {
+ * 	dreheRechts : void
+ * 	dreheLinks : void
+ * 	geradeaus() : void
+ * }
+ * 
+ * Auto - "1" Lenkrad
+ * 
+ * abstract class Pedal {
+ * Object[] elementData
+ * size()
+ * }
+ * Pedal <|-- Bremspedal
+ * Pedal <|-- Gaspedal
+ * 
+ * Auto - "1" Bremspedal
+ * Auto - "1" Gaspedal
+ * 
+
+ * @enduml
+ * @author Bettina Biendara
+ *
+ */
 public class Main {
 
 	public static void main(String[] args) {
